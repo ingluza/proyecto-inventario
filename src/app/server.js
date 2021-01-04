88470006d5606fd8ee7000864ae188683c36e613
ingluza <ingluzaura@hotmail.com>
2021-01-03 20:26:23 -0500

    Cambios para desplegar en heroku

--- src/app/server.js
+++ src/app/server.js
@@ -4,6 +4,9 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname+'/dist/app-angular'));
-app.get('/', function(req,res){
+app.get('/*', function(req,res){
     res.sendFile(path.join(__dirname+'/dist/app-angular/index.html'));
 })
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
