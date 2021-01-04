6d65394c37baca86e18c3fa9e4f47f444a1e9888
ingluza <ingluzaura@hotmail.com>
2021-01-04 18:36:10 -0500

    Cambios para el deploy

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/app-angular'));
+app.use(express.static('/dist/app-angular'));
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname+'./dist/app-angular/index.html'));
+    res.sendFile(path.join('./dist/app-angular/index.html'));
 })
 
 // Start the app by listening on the default Heroku port
