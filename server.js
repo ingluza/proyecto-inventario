6de8965d6a921b3f414c66b58d26473c81f97532
ingluza <ingluzaura@hotmail.com>
2021-01-05 13:58:03 -0500

    Cambios para el deploy

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const path = require('path');
+//Install express server
+import express, { static } from 'express';
+import { join } from 'path';
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/app-angular'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/app-angular/index.html'));
+// Serve only the static files form the dist directory
+app.use(static(__dirname + '/dist/app-angular'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(join(__dirname+'/dist/app-angular/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);                                                                               
