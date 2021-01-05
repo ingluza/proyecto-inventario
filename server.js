53080ee23a59f417ab46a5ccc364b5f53ed21829
ingluza <ingluzaura@hotmail.com>
2021-01-05 14:12:12 -0500

    Agrego archivo Procfile para Heroku

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
-import express, { static } from 'express';
-import { join } from 'path';
+const express = require('express');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(static(__dirname + '/dist/app-angular'));
+app.use(express.static('./dist/app-angular'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(join(__dirname+'/dist/app-angular/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/app-angulare' }
+  );
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);                                                                               
+app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
