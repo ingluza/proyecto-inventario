3bcfa56714d40cd586cae7ff7906991bfe987c72
ingluza <ingluzaura@hotmail.com>
2021-01-06 14:55:57 -0500

    Añadir nuevo contacto

--- src/app/views/proveedores/crearproveedores.component.ts
+++ src/app/views/proveedores/crearproveedores.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-proveedores',
@@ -6,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearProveedoresComponent implements OnInit {
 
-  constructor() { }
+  get contacto(): FormArray {
+    return this.formularioContacto.get('contacto') as FormArray;
+  }
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+
+  formularioContacto: FormGroup;
+
+  ngOnInit() {
+    this.crearFormulario();
+    this.anadirContacto();
+  }
+
+  crearFormulario() {
+    this.formularioContacto = this.fb.group({
+      contacto: this.fb.array([])
+    });
+   }
+
+   anadirContacto() {
+    const nuevoContacto = this.fb.group({
+      nombreContacto: new FormControl(''),
+      correoContacto: new FormControl(''),
+      telefonoContacto: new FormControl('')
+    });
+
+    this.contacto.push(nuevoContacto);
   }
 
 }
